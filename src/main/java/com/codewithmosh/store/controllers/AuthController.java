d8198be1381e1fdada9491a7aada1cbcf5a4fa7c
Memecon <jon.byrne27@btinternet.com>
2025-08-06 13:34:02 +0100

    Jwt starting

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -1,12 +1,16 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.JwtResponse;
 import com.codewithmosh.store.dtos.LoginDto;
 import com.codewithmosh.store.exceptions.PasswordInvalidException;
 import com.codewithmosh.store.exceptions.UserNotFoundException;
-import com.codewithmosh.store.services.AuthService;
+import com.codewithmosh.store.services.JwtService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +23,16 @@ import java.util.Map;
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody LoginDto loginDto) {
-        authService.login(loginDto);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginDto loginDto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword())
+        ); //authenticationManager takes the code from UserService that is used to validate the user exists with email = loginDto email
+        //also will check the password as well, meaning all logic is handled in spring boot security internally
+        var token = jwtService.generateToken(loginDto.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
     }
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFound() {
@@ -32,5 +41,9 @@ public class AuthController {
     @ExceptionHandler(PasswordInvalidException.class)
     public ResponseEntity<Map<String, String>> handleProductNotFound() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Password is not valid"));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
---
+++ src/main/java/com/codewithmosh/store/services/UserService.java
@@ -0,0 +1,23 @@
+package com.codewithmosh.store.services;
+
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
+        //this is a User defined in security.core.userdetails and returns a user with an email, password and list of authorities that they are granted
+    }
+}
